44ec7f04378b03c5866d05b4bc6584955f12d9e9
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-06-18 14:55:07 +0200

    Add setup.py

---
+++ aux2mongodb/__init__.py
@@ -0,0 +1,1 @@
+from .auxservices import AuxService, MagicWeather
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='aux2mongodb',
+    version='0.0.1',
+    description='A module to read in fact aux data and put them into a mongodb',
+    url='http://github.com/fact-project/aux2mongodb',
+    author='Maximilian Noethe',
+    author_email='maximilian.noethe@tu-dortmund.de',
+    license='MIT',
+    packages=['aux2mongodb', 'aux2mongodb.auxservices'],
+    package_data={
+        '': ['resources/*', 'credentials/credentials.encrypted']},
+    install_requires=[
+        'astropy',
+        'pandas',
+    ],
+    zip_safe=False,
+)
