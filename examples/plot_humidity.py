539f78c8ea4ca1692ae27a2d0bdc01004b5ad471
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-06-18 22:38:44 +0200

    Modify example to make camera vs. outside humidity plot

--- examples/plot_humidity.py
+++ examples/plot_humidity.py
@@ -1,12 +1,40 @@
 import matplotlib.pyplot as plt
-from aux2mongodb import MagicWeather
-from datetime import date
+from aux2mongodb import MagicWeather, PfMini
+import pandas as pd
+from tqdm import tqdm
+import datetime
+
+plt.style.use('ggplot')
 
 
-m = MagicWeather(auxdir='/fact/aux')
+magic_weather = MagicWeather(auxdir='/fact/aux')
+pf_mini = PfMini(auxdir='/fact/aux')
+dates = pd.date_range('2015-10-20', datetime.date.today())
 
-df = m.read_date(date(2015, 12, 31))
+outside = pd.DataFrame()
+camera = pd.DataFrame()
+for d in tqdm(dates):
+    try:
+        outside = outside.append(magic_weather.read_date(d), ignore_index=True)
+    except FileNotFoundError:
+        continue
+    try:
+        camera = camera.append(pf_mini.read_date(d), ignore_index=True)
+    except FileNotFoundError:
+        continue
 
-df.plot(x='timestamp', y='humidity', legend=False)
-plt.ylabel('Humidity / %')
+outside.set_index('timestamp', inplace=True)
+camera.set_index('timestamp', inplace=True)
+outside = outside.resample('24h').mean()
+camera = camera.resample('24h').mean()
+
+fig, ax = plt.subplots()
+ax.set_title('Camera vs. Outside Humidity (24h mean)')
+
+outside.plot(y='humidity', legend=False, label='Outside', ax=ax)
+camera.plot(y='humidity', legend=False, label='In Camera', ax=ax)
+
+ax.legend()
+ax.set_ylabel('Humidity / %')
+fig.tight_layout()
 plt.show()
